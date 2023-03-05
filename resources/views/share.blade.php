ce06ddb40574f9638988e5da2d811ed173caf512
nazalappp <nazala_270@smkassalaambandung.sch.id>
2023-03-05 13:11:33 +0700

    Profile new

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -25,15 +25,9 @@
   <div class="container-scroller">
     <!-- partial:../../partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <h3><b><i>ZanashCook</i></b></h3>
-        {{-- <a class="navbar-brand brand-logo mr-5" href="../../index.html"><img src="{{asset('assets/images/logo.svg')}}" class="mr-2" alt="logo"/></a> --}}
-        {{-- <a class="navbar-brand brand-logo-mini" href="../../index.html"><img src="{{asset('assets/images/logo-mini.svg')}}" alt="logo"/></a> --}}
-      </div>
+      
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-          <span class="icon-menu"></span>
-        </button>
+        
         <ul class="navbar-nav navbar-nav-right">
           <li class="nav-item nav-profile dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
