db9e77ba994e59c86b0b700b054f39f0648ee3eb
RefalFalah <{ID}+{RefalFalah}@users.noreply.github.com>
2023-03-05 14:45:51 +0700

    Lier si nazala wakaka :poop:

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.admin')
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>ZanashCook</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="{{asset('assets/vendors/feather/feather.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="{{asset('assets/vendors/select2/select2.min.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css')}}">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('assets/css/vertical-layout-light/style.css')}}">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" />
-</head>
-
-<body>
+@section('content')
   <div class="container-scroller">
     <!-- partial:../../partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
@@ -40,11 +19,6 @@
               <img src="{{asset('assets/images/faces/face28.jpg')}}" alt="profile"/>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-              <a href="{{ url('/profile')}}">
-                <i class="ni ni-user-run">
-                  <span>Profile</span>
-                </i>
-              </a>
               <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                 <i class="ti-power-off text-dark"></i>
                 Logout
@@ -125,28 +99,4 @@
     </div>
     <!-- page-body-wrapper ends -->
   </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="{{asset('assets/vendors/typeahead.js/typeahead.bundle.min.js')}}"></script>
-  <script src="{{asset('assets/vendors/select2/select2.min.js')}}"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="{{asset('assets/js/off-canvas.js')}}"></script>
-  <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('assets/js/template.js')}}"></script>
-  <script src="{{asset('assets/js/settings.js')}}"></script>
-  <script src="{{asset('assets/js/todolist.js')}}"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="{{asset('assets/js/file-upload.js')}}"></script>
-  <script src="{{asset('assets/js/typeahead.js')}}"></script>
-  <script src="{{asset('assets/js/select2.js')}}"></script>
-  <!-- End custom js for this page-->
-
-  @include('sweetalert::alert')
-</body>
-
-</html>
+@endsection
